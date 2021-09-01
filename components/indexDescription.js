e48f29f5434031cbac4b661abe818c96ced6e565
Miika Härmä <harmamiika@gmail.com>
2021-09-01 23:17:05 +0300

    quickcommit

---
+++ components/indexDescription.js
@@ -0,0 +1,10 @@
+export default function () {
+  return (
+    <div className="description-container">
+      <div className="text-container">Our world famous businessForum</div>
+      <div className="text-container">We have all the best businesses here</div>
+    </div>
+  );
+}
+
+// 2 boxes nicely laid out
---
+++ components/seeBusinesses.js
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <div>
+      <div className="text-container">See our businesses</div>
+    </div>
+  );
+}
